6dbd6a135f479f1dcf8b0813f888d30fe003da8d
lyt888 <625464771@qq.com>
2022-03-04 17:35:08 +0800

    第一次提交

---
+++ src/view/home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="nav">
+    <router-link to="/">首页</router-link>
+    <router-link to="/page2">page2</router-link>
+  </div>
+  <router-view></router-view>
+</template>
+<script>
+</script>
